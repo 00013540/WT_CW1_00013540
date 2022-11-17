7242c9b14df228e35b5b0ce4d0698291ff1fd99a
00013540 <wiut00013540@gmail.com>
2022-11-18 01:22:50 +0500

    feat: Add instaSport page

---
+++ db/instasportData.js
@@ -0,0 +1,75 @@
+export default {
+  data: {
+    actuals: [
+      {
+        title: "How playing sports benefits your body and your brain",
+        views: 121,
+        likes: 50,
+        link: "hmFQqjMF_f0",
+      },
+      {
+        title: "Sport against poverty - 6 Minute English",
+        views: 465,
+        likes: 232,
+        link: "atPNphv-NEI",
+      },
+      {
+        title: "Sports and Actions Talking Flashcards",
+        views: 345,
+        likes: 89,
+        link: "M_kPHSc65R4",
+      },
+    ],
+    all: [
+      {
+        title: "Sports & Activities | A Conversation about Interests",
+        views: 454,
+        likes: 232,
+        link: "ZXnHE1tZQjo",
+      },
+      {
+        title: "20-Minute Victoria Sport Workout For Toned Abs and Legs",
+        views: 655,
+        likes: 125,
+        link: "3TJP5a3pBME",
+      },
+      {
+        title: "10 MIN BOOTY BOMB WORKOUT",
+        views: 784,
+        likes: 88,
+        link: "zFGHZeCTz60",
+      },
+      {
+        title: "8 MIN UPPER BODY STRETCH",
+        views: 657,
+        likes: 468,
+        link: "0XBcrjkkwQo",
+      },
+      {
+        title: "Stretches for Neck, Shoulder & Upper Back Pain Relief",
+        views: 234,
+        likes: 127,
+        link: "s-7lyvblFNI",
+      },
+      {
+        title:
+          "How to get rid of muscle knots in your neck, traps, shoulders, and back",
+        views: 401,
+        likes: 98,
+        link: "9PC9Mn0VO00",
+      },
+      {
+        title: "Hit Workout 30 Minutes",
+        views: 395,
+        likes: 227,
+        link: "NUQ-W0tot4U",
+      },
+      {
+        title: "What will happen if you plank every day for 1 minute",
+        views: 433,
+        likes: 113,
+        link: "Bnios4sOV04",
+      },
+    ],
+  },
+};
---
+++ scripts/instasport.js
@@ -0,0 +1,65 @@
+import instasportData from "../db/instasportData.js";
+const instasportTop = document.getElementById("instasport-top");
+const instasportBottom = document.getElementById("instasport-bottom");
+
+const drawInstasport = () => {
+  const { data } = instasportData;
+  const contentTop = data.actuals.map(
+    (item) =>
+      `
+      <div class="instasport__top__box">
+        <div class="instasport__top__video">
+            <iframe
+            class="instasport__top__iframe"
+            src="https://www.youtube.com/embed/${item.link}"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+            >
+            </iframe>
+        </div>
+      <p class="instasport__top__title">${item.title}</p>
+      <div class="instasport__top__content">
+            <span class="instasport__top__views">${item.views} views</span>
+            <div class="instasport__top__likes">
+            <i class="fa-solid fa-thumbs-up instasport__top__icon"></i>
+            <span class="instasport__top__count">${item.likes}</span>
+            </div>
+      </div>
+    </div>
+      `
+  );
+
+  const contentBottom = data.all.map(
+    (item) =>
+      `
+    <div class="instasport__bottom__box">
+        <div class="instasport__bottom__video">
+        <iframe
+            class="instasport__bottom__iframe"
+            src="https://www.youtube.com/embed/${item.link}"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+        >
+        </iframe>
+        </div>
+        <p class="instasport__bottom__title">${item.title}</p>
+        <div class="instasport__bottom__content">
+        <span class="instasport__bottom__views">${item.views} views</span>
+        <div class="instasport__bottom__likes">
+            <i class="fa-solid fa-thumbs-up instasport__bottom__icon"></i>
+            <span class="instasport__bottom__count">${item.likes}</span>
+        </div>
+        </div>
+    </div>
+    `
+  );
+
+  instasportTop.innerHTML = contentTop.join("");
+  instasportBottom.innerHTML = contentBottom.join("");
+};
+
+drawInstasport();
